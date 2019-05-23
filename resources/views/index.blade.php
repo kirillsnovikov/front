2f632c0bb26cf7abf5a3591eab815c2f38132372
kirillsnovikov <kirillsnovikov@gmail.com>
2019-05-24 00:16:21 +0300

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>CCO.CC</title>
+    <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
+</head>
+<body>
+    <div id="app">
+        <div class="wrap">
+            <div class="header">
+                <div class="nav">
+                    <div class="logo"><a href="#">CCO.CC</a></div>
+                    <div class="menu">
+                        <ul class="unstyled">
+                            <li><a href="#">Фильмы</a></li>
+                            <li><a href="#">Мультфильмы</a></li>
+                            <li><a href="#">Сериалы</a></li>
+                            <li><a href="#">ТВ</a></li>
+                        </ul>
+                    </div>
+                    <div class="right-nav">
+                        <div class="search-components">
+                            <form>
+                                <input type="text" class="search">
+                                <button>O</button>
+                            </form>
+                            <a href="#">Расширенный поиск</a>
+                        </div>
+                        <a href="#"><div>Sign-In</div></a>
+                    </div>
+                </div>
+            </div>
+            <div class="main-content">
+                <div class="compilation">
+                    <div class="compilation-header">
+                        <h2>Заголовок подборки фильмов</h2>
+                    </div>
+                    <div class="compilation-slider">
+                        <div class="compilation-items">
+                            <div class="compilation-item">
+                                <div class="card">
+                                    <div class="card-poster"><img src="https://loremflickr.com/240/320/art?random=1" alt="Постер к фильму ########"></div>
+                                    <div class="card-title">Название фильма</div>
+                                    <div class="card-raiting">Кинопоиск: 7.9987</div>
+                                </div>
+                            </div>
+                            <div class="compilation-item">
+                                <div class="card">
+                                    <div class="card-poster"><img src="https://loremflickr.com/240/320/art?random=2" alt="Постер к фильму ########"></div>
+                                    <div class="card-title">Название фильма</div>
+                                    <div class="card-raiting">Кинопоиск: 7.9987</div>
+                                </div>
+                            </div>
+                            <div class="compilation-item">
+                                <div class="card">
+                                    <div class="card-poster"><img src="https://loremflickr.com/240/320/art?random=3" alt="Постер к фильму ########"></div>
+                                    <div class="card-title">Название фильма</div>
+                                    <div class="card-raiting">Кинопоиск: 7.9987</div>
+                                </div>
+                            </div>
+                            <div class="compilation-item">
+                                <div class="card">
+                                    <div class="card-poster"><img src="https://loremflickr.com/240/320/art?random=4" alt="Постер к фильму ########"></div>
+                                    <div class="card-title">Название фильма</div>
+                                    <div class="card-raiting">Кинопоиск: 7.9987</div>
+                                </div>
+                            </div>
+                            <div class="compilation-item">
+                                <div class="card">
+                                    <div class="card-poster"><img src="https://loremflickr.com/240/320/art?random=5" alt="Постер к фильму ########"></div>
+                                    <div class="card-title">Название фильма</div>
+                                    <div class="card-raiting">Кинопоиск: 7.9987</div>
+                                </div>
+                            </div>
+                            <div class="compilation-item">
+                                <div class="card">
+                                    <div class="card-poster"><img src="https://loremflickr.com/240/320/art?random=6" alt="Постер к фильму ########"></div>
+                                    <div class="card-title">Название фильма</div>
+                                    <div class="card-raiting">Кинопоиск: 7.9987</div>
+                                </div>
+                            </div>
+                            <div class="compilation-item">
+                                <div class="card">
+                                    <div class="card-poster"><img src="https://loremflickr.com/240/320/art?random=7" alt="Постер к фильму ########"></div>
+                                    <div class="card-title">Название фильма</div>
+                                    <div class="card-raiting">Кинопоиск: 7.9987</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer">footer</div>
+        </div>
+    </div>
+</body>
+</html>
